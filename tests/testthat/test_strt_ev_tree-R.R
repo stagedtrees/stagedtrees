2c61852d897a89f116fe1de9950043c40c7cbe53
gherardovarando <gherardo.varando@gmail.com>
2018-10-03 17:53:23 +0200

    [upd] update doc and minor fix

--- tests/testthat/test_strt_ev_tree-R.R
+++ tests/testthat/test_strt_ev_tree-R.R
@@ -1,8 +1,19 @@
 context("basic model creation of stratified event trees")
 
+test_that("test that the creator works for list",{
+  ev <- strt_ev_tree.list(x = list(A=c(1,2), B =c("x","y"), C=c("3","4")))
+  expect_is(ev,"strt_ev_tree")
+})
+
+test_that("test that the creator works for data.frame",{
+  DD <- data.frame(A = as.factor(c(1,2,2,1)), B = as.factor(c("a","b","a","b")))
+  ev <- strt_ev_tree(x = DD,order = c("B","A"))
+  expect_is(ev,"strt_ev_tree")
+})
+
 test_that("test that the stratified event tree is created with the right order I", {
 
-  ev <- strt_ev_tree.list(x = list(A=c(1,2), B =c("x","y"), C=c("3","4")))
+  ev <- strt_ev_tree(x = list(A=c(1,2), B =c("x","y"), C=c("3","4")))
   expect_true(all(names(ev$tree) == c("A","B","C")))
 
 })
@@ -10,8 +21,18 @@ test_that("test that the stratified event tree is created with the right order I
 test_that("test that the stratified event tree is created with the right order II", {
 
   DD <- data.frame(A = as.factor(c(1,2,2,1)), B = as.factor(c("a","b","a","b")))
-  ev <- strt_ev_tree.data.frame(x = DD,order = c("B","A"))
+  ev <- strt_ev_tree(x = DD,order = c("B","A"))
   expect_true(all(names(ev$tree) == c("B","A")))
+
+})
+
+context("fitting of stratified event tree")
+
+test_that("test that the stratified event tree is fitted", {
+
+  DD <- data.frame(A = as.factor(c(1,2,2,1)), B = as.factor(c("a","b","a","b")))
+  ev <- strt_ev_tree( x = DD, fit = TRUE)
+  expect_equal(ev$prob$A, table(DD[ "A" ], dnn = c( "A" )) / sum(table(DD[ "A" ])))
 
 })
 
