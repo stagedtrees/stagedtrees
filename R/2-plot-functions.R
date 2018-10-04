0f6c0add8ee56dfd73ced65a3dd330c2cc096e95
gherardovarando <gherardo.varando@gmail.com>
2018-10-04 16:08:12 +0200

    [upd] plotting works, now paths are ordered lexicographically

--- R/2-plot-functions.R
+++ R/2-plot-functions.R
@@ -43,9 +43,58 @@ plot.strt_ev_tree <- function(evt, rmax=1, rmin= 0.1, step = 2, ...){
 }
 
 
-node <- function(x, r, label = "", col = "black", ...){
+#' plot a staged event tree
+#'
+#' @param x event tree object
+#' @param ...
+#' @export
+#' @importFrom graphics lines plot.new plot.window
+
+plot.staged_ev_tree <- function(evt, rmax=1, rmin= 0.1, step = 2, ...){
+  plot.new()
+  plot.window(xlim=c(0,step*length(evt$tree)),
+              ylim=c(-step*length(evt$tree)/2-0.5,step*length(evt$tree)/2+0.5),
+              asp=1, ...)
+  n <- evt$tree
+  nms <- names(evt$tree)
+  d <- length(evt$tree)
+  node(c(0,0),rmax,label = nms[1]) #plot first node
+  ns <- 1
+  x<-0
+  y<-0
+  yy <- 0
+  coef <- 1
+  for (k in 1:(length(evt$tree)-1)){ #plot nodes for every strata
+    v <- evt$tree[[k]]
+    yyy <- yy
+    yy <- c()
+    lj <- 0
+    x <- step*k #increase x position
+    nv <- length(v)
+    for (i in 1:ns){ #for every old node
+      y <- yyy[i] + (step/2)*coef*d*seq(from=-1, to = 1, length.out = nv)/(ns * nv  ) #compute new y positions
+      yy <- c(yy,y)
+      for (j in 1:nv){ #plot nodes
+        lj <- lj +1
+        dk <- dim(evt$paths[[k]])[2]
+        ### not working well
+        node(c(x, y[j]), rmax/sqrt(nv*ns), label = nms[k+1], col = evt$paths[[k]][lj , dk])
+        edge(c(x-step, yyy[i]), c(x,y[j]), v[j] ) #plot edge with previous nodes
+      }
+    }
+    ns <- ns*nv
+    if ( nv %% 2 == 1   ) { ## this is to avoid overlapping,
+      coef <- coef*0.4      ## not sure why it works like this but ok
+    } else {
+      coef <- 1*coef
+    }
+  }
+}
+
+
+node <- function(x, r, label = "", col = "black", cex.label = 2*sqrt(r),...){
   circle(x,r, col=col,...)
-  text(x = x[1],y = x[2],labels = label, col=col,...)
+  text(x = x[1],y = x[2],labels = label, col=col, cex=cex.label,...)
 }
 
 edge <- function(from, to, label,col="black",...){
