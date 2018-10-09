a72d0c1b575b16147eaebd9fda75d54077b50866
gherardovarando <gherardo.varando@gmail.com>
2018-10-09 14:23:04 +0200

    [+] CHDS dataset

---
+++ R/data-doc.R
@@ -0,0 +1,13 @@
+#' Child Health and Development Studies
+#'
+#' A dataset containing ... (to do)
+#'
+#' @format A data frame with 890 rows and 4 variables:
+#' \describe{
+#'   \item{Social}{Binary variable}
+#'   \item{Economic}{Binary variable}
+#'   \item{Events}{Ternary variable}
+#'   \item{Admission}{Binary variable}
+#' }
+#' @source \url{FIND URL}
+"CHDS"
