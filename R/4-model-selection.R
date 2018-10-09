6d303fa629760d8ec6b4db7d56cda5b2959496e1
gherardovarando <gherardo.varando@gmail.com>
2018-10-09 11:56:33 +0200

    [upd] random hill climb and hill climb

--- R/4-model-selection.R
+++ R/4-model-selection.R
@@ -22,7 +22,9 @@ forward_select.staged_ev_treee <- function(object=NULL, data=NULL, lambda = 1,
 
 }
 
-#' backword model selectio
+#' backword naive random hill-climbing
+#'
+#' Randomly select a model and selct it if it increase the score
 #'
 #' @param object a staged event tree model (optional)
 #' @param data the data (can be not specified if it is attached to \code{object})
@@ -33,7 +35,7 @@ forward_select.staged_ev_treee <- function(object=NULL, data=NULL, lambda = 1,
 #' @param max_iter the maximum number of iteration
 #' @param verbose If info should be printed
 #' @export
-backward_select.staged_ev_tree <- function(object = NULL, data = NULL, order = NULL
+backward_hill_climb_random.staged_ev_tree <- function(object = NULL, data = NULL, order = NULL
                                            , lambda=1
                                            , score = function(x) return( - BIC(x) )
                                            , eps = 0.0001, max_iter = 100
@@ -77,3 +79,74 @@ backward_select.staged_ev_tree <- function(object = NULL, data = NULL, order = N
   if (verbose){ print(paste("Exit after", iter, "iteration"))}
   return(object)
 }
+
+
+#' backword hill-climbing
+#'
+#' Each iter move to the best model
+#'
+#' @param object a staged event tree model (optional)
+#' @param data the data (can be not specified if it is attached to \code{object})
+#' @param order the order of the tree (optional)
+#' @param lambda the laplace smoothing factor
+#' @param score the score function to be maximized
+#' @param eps the stopping criteria for the relative score increase
+#' @param max_iter the maximum number of iteration
+#' @param verbose If info should be printed
+#' @export
+backward_hill_climb.staged_ev_tree <- function(object = NULL, data = NULL, order = NULL
+                                                      , lambda=1
+                                                      , score = function(x) return( - BIC(x) )
+                                                      , eps = 0.0001, max_iter = 100
+                                                      , verbose = FALSE){
+  if (is.null(object)){
+    if (is.null(data)){
+      warning("Provide something: the fitted staged event tree or data")
+      return(NULL)
+    }
+    ## if the staged event tree is not provided initialize it to the full model
+    object <- staged_ev_tree(strt_ev_tree(data, fit = TRUE,
+                                          order = order, lambda = lambda))
+  }
+  if (is.null(data)){
+    data <- sevt$data
+    if (is.null(data)){
+      warning("Provide data")
+      return(object)
+    }
+  }
+  now_score <- score(object)
+  r <- 1
+  iter <- 0
+  while (r > eps && iter < max_iter){ ## chose randomly one of the variable and try to perform a stage-merging
+    iter <- iter + 1
+    temp <- object #clone the object
+    temp_score <- now_score
+    for (v in names(object$tree)[-1]){
+      if (length(object$stages[[ v ]]) > 1 ){
+        for (i in 2:length(object$stages[[ v ]])){ ##try all stages pair
+          s1 <- object$stages[[ v ]][i]
+          for (j in 1:(i-1)){
+             s2 <- object$stages[[ v ]][j]
+             try <- join_stages(object, v, s1, s2) ## join the 2 stages
+             try_score <- score(try)
+             if (try_score > now_score){
+               temp <- try
+               r <- abs((try_score - now_score) / now_score) ##compute relative score increase
+               temp_score <- try_score
+             }
+          }
+        }
+      } ##end if there are more than 1 stage
+    } ## end for over variables
+    object <- temp
+    now_score <- try_score
+    if (verbose){
+      print(paste("joined stages:"))
+    }
+  } ## end while
+  if (verbose){ print(paste("Exit after", iter, "iteration"))}
+  return(object)
+}
+
+
