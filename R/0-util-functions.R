edf7e853962c0abea637cbfdad7d44a15beaaf0f
gherardovarando <gherardo.varando@gmail.com>
2018-11-01 14:07:28 +0100

    [upd}

--- R/0-util-functions.R
+++ R/0-util-functions.R
@@ -1,34 +1,36 @@
 
+
 # find the stage in the path
 # no checking, be careful how to use it
 # to do implement TEST
 # paths is a data.frame as the ones obtained with expand.grid
 # plus one last column with the stage index.
 # path is a vector of length = dim(paths)[2] - 1
-find_stage <- function(paths, path){
+find_stage <- function(paths, path) {
   k <- dim(paths)[2]
-  for (i in 1:(dim(paths)[1])){
-    if (all(paths[i,-k] == path[1:(k-1)])) return(as.character(paths[i,k]))
+  for (i in 1:(dim(paths)[1])) {
+    if (all(paths[i, -k] == path[1:(k - 1)]))
+      return(as.character(paths[i, k]))
   }
 }
 
 # find the paths for a given stage index
 # paths is a data.frame as the ones obtained with expand.grid
 # stage is an integer, the stage index
-find_paths <- function(paths, stage){
-  return(paths[paths[,dim(paths)[2]] == as.character(stage), ])
+find_paths <- function(paths, stage) {
+  return(paths[paths[, dim(paths)[2]] == as.character(stage),])
 }
 
 #' Compute the KL matrix
 #'
 #' @param x conditional probability table
 #' @return The matrix witht the symmetric KL (KL(i,j) + KL(j,i))
-KL_mat_prob <- function(x){
+KL_mat_prob <- function(x) {
   d <- dim(x)[1]
-  M <- matrix(nrow = d, ncol=d)
-  for (i in 1 : d){
-    for (j in 1 : d){
-      M[i, j] <- sum( x[i,] * (log(x[i,])-log(x[j,])) )
+  M <- matrix(nrow = d, ncol = d)
+  for (i in 1:d) {
+    for (j in 1:d) {
+      M[i, j] <- sum(x[i, ] * (log(x[i, ]) - log(x[j, ])))
     }
   }
   return(M + t(M))
@@ -39,12 +41,12 @@ KL_mat_prob <- function(x){
 #' Compute the KL matrix
 #' @param x list of conditional probabilities for each stage
 #' @return The matrix witht the symmetric KL (KL(i,j) + KL(j,i))
-KL_mat_stages <- function(x){
+KL_mat_stages <- function(x) {
   d <- length(x)
-  M <- matrix(nrow = d, ncol=d)
-  for (i in 1 : d){
-    for (j in 1 : d){
-      M[i, j] <- sum( x[[i]] * (log(x[[i]])-log(x[[j]])) )
+  M <- matrix(nrow = d, ncol = d)
+  for (i in 1:d) {
+    for (j in 1:d) {
+      M[i, j] <- sum(x[[i]] * (log(x[[i]]) - log(x[[j]])))
     }
   }
   return(M + t(M))
@@ -56,31 +58,125 @@ KL_mat_stages <- function(x){
 #' from a distance (positive, symmetric) matrix
 #' @param M matrix of distance
 #' @return a list with component \code{I} and \code{J} (the two group of clusters)
-simple_clustering <- function(M){
+simple_clustering <- function(M) {
   ##first we find the two indxs that return the maximum distance.
   idx <- which.max(M)
   i <- ceiling(idx / dim(M)[1])
-  j <- idx - (i-1)*dim(M)[1]
-  todo <- (1 : (dim(M)[1]) )[-c(i,j)] ## index to assign to cluster
+  j <- idx - (i - 1) * dim(M)[1]
+  todo <- (1:(dim(M)[1]))[-c(i, j)] ## index to assign to cluster
   I <- c(i) #initialize clusters
   J <- c(j) #initialize clusters
-  for (k in todo){ ## assign other indxs to the closest cluster
-     if (M[k,i] < M[k,j]){
-       I <- c(I, k)
-     }else{
-       J <- c(J, k)
-     }
+  for (k in todo) {
+    ## assign other indxs to the closest cluster
+    if (M[k, i] < M[k, j]) {
+      I <- c(I, k)
+    } else{
+      J <- c(J, k)
+    }
   }
-  return(list(I = I, J =J , i =i , j =j ))
+  return(list(
+    I = I,
+    J = J ,
+    i = i ,
+    j = j
+  ))
 }
 
 #' compute the entropy
 #' @param p the vector of normalized probability
 #' @return the entropy as \code{-sum(p*log(p))}
-entr <- function(p){
-  return(-sum(p*log(p)))
+entr <- function(p) {
+  return(-sum(p * log(p)))
 }
 
-kl <- function(p, q){
-  return( sum( p *(log(p) - log(q))))
+kl <- function(p, q) {
+  return(sum(p * (log(p) - log(q))))
 }
+
+#' noisy xor function
+#'
+#' @param x a vector of +1 nad -1
+#' @param eps the uniform noise amount
+#' @return the computed noisy xor
+xor <- function(x, eps = 0) {
+  #set.seed(seed = 0)
+  return(sign(prod(x) + runif(
+    n = 1, min = -eps, max = eps
+  )))
+}
+
+#' generate a xor dataset
+#'
+#' @param n number of variables
+#' @param N number of observations
+#' @param eps error
+#'
+#' @return
+generate_xor_dataset <- function(n = 2,
+                                 N = 100,
+                                 eps = 1.2) {
+  DD <- data.frame(observation = 1:N)
+  for (i in 1:n) {
+    DD[[paste("X", i, sep = "")]] <- sample(
+      c(-1, +1),
+      #prob=rep(1,2),
+      prob = runif(2, min = 0, max = 1),
+      size = N,
+      replace = TRUE
+    )
+  }
+  DD$C <- apply(
+    DD,
+    MARGIN = 1,
+    FUN = function(x) {
+      return(xor(x = x[-1], eps = eps))
+    })
+  DD <- as.data.frame(sapply(
+    DD,
+    FUN = function(x) {
+      factor(x, levels = c(-1, 1))
+    }
+  ))
+  return(DD[,-1])
+}
+
+
+### TO CHANGE DO NOT USE
+generate_random_dataset <-
+  function(n = 2,
+           N = 10000,
+           eps = 1.2,
+           gamma = runif(1, min = -n, max = n),
+           alpha = runif(n, min = -n, max = n),
+           beta = runif(n, min = -1, max = 1)) {
+    DD <- data.frame(observation = 1:N)
+    for (i in 1:n) {
+      DD[[paste("X", i, sep = "")]] <- sample(c(-1, +1),
+                                              prob = rep(1, 2),
+                                              #prob=runif(4,min = 1,max = 1),
+                                              size = N,
+                                              replace = TRUE)
+    }
+    DD$C <- apply(
+      DD,
+      MARGIN = 1,
+      FUN = function(x) {
+        return(sign(
+          sum(x[-1] * alpha) + sum((beta * x[-1]) %*% t(x[-1])) + runif(
+            n = 1,
+            min = -eps,
+            max = eps
+          ) + gamma
+        ))
+      }
+    )
+    DD$C[DD$C == 0] <- 1
+    datafactor <- as.data.frame(sapply(DD, FUN = as.factor))
+    return(list(
+      data = DD[, -1],
+      datafactor = datafactor[, -1],
+      alpha = alpha ,
+      gamma = gamma,
+      beta = beta
+    ))
+  }
