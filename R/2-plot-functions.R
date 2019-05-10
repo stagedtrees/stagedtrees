cc7ada20d93a35cca06c417dc269f53b5c39493a
gherardovarando <gherardo.varando@gmail.com>
2019-05-10 18:05:20 +0200

    [f] bugs

--- R/2-plot-functions.R
+++ R/2-plot-functions.R
@@ -9,7 +9,8 @@
 #' @export
 #' @importFrom graphics lines plot.new plot.window
 
-plot.strt_ev_tree <- function(x, rmax=1, rmin= 0.1, step = 2, limit = 10,
+plot.strt_ev_tree <- function(x, rmax=1, rmin= 0.1, step = 2, 
+                              limit = 10,
                               ...){
   plot.new()
   d <- min(length(x$tree), limit) ##avoid too much plotting
