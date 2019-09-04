93224c5de377d581ab0f8d5b40ad6edcbefe83d4
FedericoCarli <47533069+FedericoCarli@users.noreply.github.com>
2019-09-04 15:18:40 +0200

    test for summary.sevt

--- tests/testthat/test_info_functions.R
+++ tests/testthat/test_info_functions.R
@@ -15,3 +15,16 @@ test_that("test that nvar.sevt return number of var", {
   expect_equal(nvar.sevt(ev), 1)
   
 })
+
+test_that("test that summary.sevt works properly with not-increasing numbering of stages", {
+  
+  model <- bhc.sevt(full(PhDArticles, lambda = 1))
+  summ <- summary(model)
+  
+  expect_equal(NROW(summ$stages.info$Mentor), length(unique(model$stages$Mentor)))
+  
+  expect_equal(as.numeric(summ$stages.info$Mentor[summ$stages.info$Mentor$stage == "6", -c(1:3)]), as.numeric(model$prob$Mentor$`6`))
+  
+  expect_equal(summ$stages.info$Married[summ$stages.info$Married$stage == "10", 3], attr(model$prob$Married$`10`, "n"))
+  
+})
