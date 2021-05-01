6a07ea5a86e7690ae0c1c93d062d6225b3c4afbe
gherardovarando <gherardo.varando@gmail.com>
2021-05-01 10:28:42 +0200

    confint.sevt fixed and tests

--- R/3-inference-functions.R
+++ R/3-inference-functions.R
@@ -144,3 +144,135 @@ logLik.sevt <- function(object, ...) {
   class(ll) <- "logLik"
   return(ll)
 }
+
+
+
+#' Confidence intervals for staged event tree parameters
+#'
+#' Confint method for class \code{sevt}.
+#'
+#' @param object an object of class \code{sevt}.
+#' @param parm 	a specification of which parameters are to be given 
+#'              confidence intervals, either a vector of numbers 
+#'              or a vector of names. If missing, all parameters are considered.
+#' @param level the confidence level required.
+#' @param sides a character string specifying the side of the confidence interval, must be one of "two.sided" 
+#' (default), "left" or "right".
+#' @param method a character string specifing which method to use: "goodman", "wald", "waldcc" or "wilson".
+#' @param ignore vector of stages which will be ignored, 
+#'               by default the name of the unobserved stages stored in 
+#'               \code{object$name_unobserved}.
+#' @param ... additional argument(s) for methods.
+#' @details Compute confidence intervals for staged event trees. By default, 
+#'          it computes two-sided confidence intervals at 95% level for the 
+#'          whole tree given in \code{object} with the wald method. 
+#' @return An object of class \code{confint.sevt} containing the desired confidence intervals.
+#' @references Goodman, L. A. (1965) On Simultaneous Confidence Intervals for Multinomial Proportions Technometrics, 7, 247-254.
+#' @references Wald, A. Tests of statistical hypotheses concerning several parameters when the number of observations is large, Trans. Am. Math. Soc. 54 (1943) 426-482.
+#' @references Wilson, E. B. Probable inference, the law of succession and statistical inference, J.Am. Stat. Assoc. 22 (1927) 209-212.
+#' @author The function is partially inspired by code in the 
+#'         \code{MultinomCI} function from the \pkg{DescTools} package, 
+#'         implemented by Andri Signorelli and Pablo J. Villacorta Iglesias.  
+#' @examples
+#' m1 <- stages_bj(full(PhDArticles), distance = "kullback", thr = 0.01)
+#' confint(m1, "Prestige", level = 0.90, sides = "left")
+#' confint(m1, "Married")
+#' confint(m1, c("Married", "Kids"))
+#' @importFrom stats confint
+#' @importFrom stats qchisq
+#' @export
+confint.sevt <- function (object, parm, level = 0.95,
+                          sides = c("two.sided", "left", "right"), 
+                          method = c("goodman", "wald", "waldcc", "wilson"),
+                          ignore = object$name_unobserved,
+                          ...) {
+  check_sevt(object)
+  check_sevt_fit(object)
+  vv <- sevt_varnames(object)
+  if (missing(parm)){
+    parm <- vv
+  }else if (is.numeric(parm)){
+    parm <- vv[parm]
+  }
+  ## order parm and remove NA
+  parm <- vv[vv %in% parm]
+  
+  ## expand parm
+  exparm <- unlist(sapply(parm, function(v){
+    s <- unique(object$stages[[v]])
+    s <- s[!(s %in% ignore)]
+    if (is.null(s)) s <- "1"
+    apply(expand.grid(object$tree[[v]],s),1, function(x){
+      paste0(v,"=", x[1],"|", x[2])
+    })
+  }, simplify = FALSE), use.names = FALSE)
+  
+  if (missing(method)) {
+    method <- "wald"
+  }
+  if (missing(sides)) {
+    sides <- "two.sided"
+  }
+  
+  sides <- match.arg(sides, choices = c("two.sided", "left", "right"), several.ok = FALSE)
+  if (sides != "two.sided") {
+    level <- 1 - 2 * (1 - level)
+  }
+  
+  lambda <- object$lambda
+  method <- match.arg(arg = method, choices = c("goodman", "wald", "waldcc", "wilson"))
+  
+  a <- (1 - level)/2
+  a <- c(a, 1 - a)
+  pct <- paste0(format(100*a,digits = 3, ), " %")
+  ci <- array(NA_real_, dim = c(length(exparm), 2L), 
+              dimnames = list(exparm, pct))
+  
+  for(v in parm) {
+    stages <- unique(object$stages[[v]]) 
+    stages <- stages[!(stages %in% ignore)]
+    if (is.null(stages)) stages <- "1"
+    k <- length(object$tree[[v]])
+    for(s in stages) {
+      p <- object$prob[[v]][[s]]
+      n <- attr(p, "n")
+      n_stage <- p * (n + k * lambda) - lambda
+      if (method == "goodman") {
+        q.chi <- qchisq(level, k - 1)
+        lci <- (q.chi + 2 * n_stage  - sqrt(q.chi * (q.chi + 4 * n_stage * 
+                                               (n - n_stage)/n)))/(2 * (n + q.chi))
+        uci <- (q.chi + 2 * n_stage + sqrt(q.chi * (q.chi + 4 * n_stage * 
+                                                      (n - n_stage)/n)))/(2 * (n + q.chi))
+      }
+      else if (method == "wald") {
+        q.chi <- qchisq(level, 1)
+        lci <- p - sqrt(q.chi * p * (1 - p)/n)
+        uci <- p + sqrt(q.chi * p * (1 - p)/n)
+      }
+      else if (method == "waldcc") {
+        q.chi <- qchisq(level, 1)
+        lci <- p - sqrt(q.chi * p * (1 - p)/n) - 1/(2 * n)
+        uci <- p + sqrt(q.chi * p * (1 - p)/n) + 1/(2 * n)
+      }
+      else if (method == "wilson") {
+        q.chi <- qchisq(level, 1)
+        lci <- (q.chi + 2 * n_stage - sqrt(q.chi^2 + 4 * n_stage * q.chi * 
+                                             (1 - p)))/(2 * (q.chi + n))
+        uci <- (q.chi + 2 * n_stage + sqrt(q.chi^2 + 4 * n_stage * q.chi * 
+                                             (1 - p)))/(2 * (q.chi + n))
+      }
+      
+      if (sides == "left") {
+        uci[] <- 1
+      }
+      else if (sides == "right") {
+        lci[] <- 0
+      }
+      ci[paste0(v,"=", object$tree[[v]],"|", s),1] <- pmax(0,lci)
+      ci[paste0(v,"=", object$tree[[v]],"|", s),2] <- pmin(1,uci)
+      
+    }
+  }
+  return(ci)
+}
+
