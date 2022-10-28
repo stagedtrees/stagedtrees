48332b2c26a9f4d3fa97ab0c37bc6c7eb8a9ed30
gherardo <gherardo.varando@gmail.com>
2022-10-28 10:10:20 +0200

    more testing with NAs

--- tests/testthat/test_model_selection.R
+++ tests/testthat/test_model_selection.R
@@ -1,8 +1,16 @@
 context("model selection")
 
 data("PhDArticles")
+### introduce some NAs
+DDna <- PhDArticles
+DDna[sample(nrow(DDna), 10), 1] <- NA
+DDna[sample(nrow(DDna), 10), 2] <- NA
+DDna[sample(nrow(DDna), 10), 3] <- NA
+
 f <- full(PhDArticles[, 1:4])
+fna <- full(DDna[, 1:4])
 ind <- indep(PhDArticles[, 1:4])
+indna <- indep(DDna[, 1:4])
 fl <- full(PhDArticles[, 1:4], lambda = 1)
 
 test_that("hc from full", {
@@ -14,9 +22,27 @@ test_that("hc from full", {
   expect_equal(ll1, ll2)
 })
 
+test_that("hc from full (NAs)", {
+  expect_silent(mod <- stages_hc(fna, max_iter = 3, scope = "Kids"))
+  expect_message(mod <- stages_hc(fna, max_iter = 3, trace = 2))
+  ll1 <- logLik(mod)
+  mod$ll <- NULL
+  ll2 <- logLik(mod)
+  expect_equal(ll1, ll2)
+})
+
 test_that("hc from indep", {
   expect_silent(mod <- stages_hc(ind, max_iter = 3, scope = "Kids"))
   expect_message(mod <- stages_hc(ind, max_iter = 3, trace = 2))
+  ll1 <- logLik(mod)
+  mod$ll <- NULL
+  ll2 <- logLik(mod)
+  expect_equal(ll1, ll2)
+})
+
+test_that("hc from indep (NAs)", {
+  expect_silent(mod <- stages_hc(indna, max_iter = 3, scope = "Kids"))
+  expect_message(mod <- stages_hc(indna, max_iter = 3, trace = 2))
   ll1 <- logLik(mod)
   mod$ll <- NULL
   ll2 <- logLik(mod)
@@ -33,6 +59,14 @@ test_that("bhc", {
   expect_equal(ll1, ll2)
 })
 
+test_that("bhc (na)", {
+  expect_silent(mod <- stages_bhc(fna, max_iter = 5, scope = "Kids"))
+  expect_message(mod <- stages_bhc(fna, max_iter = 5, trace = 2))
+  ll1 <- logLik(mod)
+  mod$ll <- NULL
+  ll2 <- logLik(mod)
+  expect_equal(ll1, ll2)
+})
 
 test_that("fbhc", {
   expect_silent(mod <- stages_fbhc(f, max_iter = 5, scope = "Kids"))
@@ -62,9 +96,26 @@ test_that("bj", {
   expect_equal(ll1, ll2)
 })
 
+test_that("bj (na)", {
+  expect_silent(mod <- stages_bj(fna, scope = "Kids"))
+  expect_message(mod <- stages_bj(fna, trace = 2))
+  ll1 <- logLik(mod)
+  mod$ll <- NULL
+  ll2 <- logLik(mod)
+  expect_equal(ll1, ll2)
+})
+
 
 test_that("join.unobserved", {
   expect_message(mod <- join_unobserved(f, trace = 2, name = "zeros"))
+  ll1 <- logLik(mod)
+  mod$ll <- NULL
+  ll2 <- logLik(mod)
+  expect_equal(ll1, ll2)
+})
+
+test_that("join.unobserved (NAs)", {
+  expect_message(mod <- join_unobserved(fna, trace = 2, name = "zeros"))
   ll1 <- logLik(mod)
   mod$ll <- NULL
   ll2 <- logLik(mod)
