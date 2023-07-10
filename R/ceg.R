24a32e2c11867e9565eb1541f273cbf7e984f1a1
gherardo varando <gherardo.varando@gmail.com>
2023-07-10 21:21:27 +0200

    fix ceg indexing of stages and positions

--- R/ceg.R
+++ R/ceg.R
@@ -13,45 +13,44 @@
 #' @export
 ceg <- function(object) {
   check_sevt(object)
+  vars <- names(object$tree)
   positions <- object$stages
-  ls <- length(object$tree) - 1
-  if (ls == 0){
-    object$positions <- list()
-    class(object) <- c("ceg", class(object))
-    return(object)
+  ls <- length(object$tree)
+  if (ls >= 2) {
+    old <- unique(object$stages[[vars[ls]]])
+    new <- paste0(seq_along(old))
+    positions[[vars[ls]]] <- vapply(object$stages[[vars[ls]]],
+      FUN = function(s) {
+        new[which(old == s, useNames = FALSE)]
+      },
+      FUN.VALUE = "a", USE.NAMES = FALSE
+    )
   }
-  us <- unique(object$stages[[ls]])
-  positions[[ls]] <- vapply(object$stages[[ls]],
-    FUN.VALUE = "a",
-    FUN = function(s) {
-      as.character(which(us %in% s))
-    }, USE.NAMES = FALSE
-  )
-  if (ls>1){
-    for (i in (ls - 1):1) {
+  if (ls > 2) {
+    for (i in (ls - 1):2) {
+      nv <- length(object$tree[[i]])
+      positions[[vars[i]]][] <- NA
+      positions[[vars[i]]][1] <- 1
       temp <- 2
-      nv <- length(object$tree[[i + 1]])
-      positions[[i]][] <- NA
-      positions[[i]][1] <- 1
-      for (j in 2:length(positions[[i]])) {
+      for (j in 2:length(positions[[vars[i]]])) {
         nj <- (nv * (j - 1) + 1):(nv * j)
         for (k in 1:(j - 1)) {
           nk <- (nv * (k - 1) + 1):(nv * k)
-          if (object$stages[[i]][j] == object$stages[[i]][k] &&
-              all(positions[[i + 1]][nj] == positions[[i + 1]][nk])) {
-            positions[[i]][j] <- positions[[i]][k]
+          if (object$stages[[vars[i]]][j] == object$stages[[vars[i]]][k] &&
+            all(positions[[vars[i + 1]]][nj] == positions[[vars[i + 1]]][nk])) {
+            positions[[vars[i]]][j] <- positions[[vars[i]]][k]
           }
         }
-        if (is.na(positions[[i]][j])) {
-          positions[[i]][j] <- temp
+        if (is.na(positions[[vars[i]]][j])) {
+          positions[[vars[i]]][j] <- paste0(temp)
           temp <- temp + 1
         }
       }
     }
   }
-  positions[[names(object$tree)[1]]] <- "1"
+  positions[[vars[1]]] <- "1"
   object$positions <- positions
-  object$positions <- object$positions[c(ls + 1, 1:ls)]
+  object$positions <- object$positions[vars]
   class(object) <- c("ceg", class(object))
   return(object)
 }
--- tests/testthat/test-stages_simplebhc.R
+++ tests/testthat/test-stages_simplebhc.R
@@ -1,7 +1,9 @@
 test_that("stages_simplebhc", {
   expect_silent(mod <- stages_simplebhc(full(PhDArticles[, 1:4])))
   mod_c <- ceg(mod)
-  expect_identical(stndnaming(mod)$stages, mod_c$positions[-1])
+  vars <- names(mod$tree)
+  expect_identical(stndnaming(mod)$stages[vars[-1]],
+                   mod_c$positions[vars[-1]])
   ll1 <- logLik(mod)
   mod$ll <- NULL
   ll2 <- logLik(mod)
