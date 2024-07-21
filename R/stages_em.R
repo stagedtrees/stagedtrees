e0fb0e56bf87d2f9f73c4a5cd29a71f2bc32adb5
gherardo varando <gherardo.varando@gmail.com>
2024-07-21 11:47:25 +0200

    seft fit em

--- R/stages_em.R
+++ R/stages_em.R
@@ -4,7 +4,7 @@
 #'               and data, as returned by \code{\link{full}}
 #'               or \code{\link{sevt_fit}}.
 #' @param data the data with missing values to be used in the EM algorithm.
-#' @param alg_em a function that performs stages structure estimation. Similar to
+#' @param stages_alg a function that performs stages structure estimation. Similar to
 #'            \code{\link{stages_bhc}} or \code{\link{stages_hclust}}.
 #'            The function \code{alg} must accept the argument
 #'            \code{scope}.
@@ -14,22 +14,24 @@
 #'                    previous step. If \code{FALSE}, at each iteration
 #'                    \code{alg_em} is initialized on the
 #'                    initial argument \code{object}.
-#' @param chain logical. If \code{TRUE} chain predictions are used to impute
+#' @param chain_impute logical. If \code{TRUE} chain predictions
+#'              (using the order of the variables in \code{object})
+#'              are used to impute
 #'              missing values. Otherwise, independent
 #'              predictions for each missing values are used.
 #' @param ... additional arguments passed to \code{alg}.
 #' @returns The final staged event tree obtained.
 #' @export
-stages_em <- function(object, data = object$data_raw, alg_em = stages_bhc,
+stages_em <- function(object, data = object$data_raw, stages_alg = stages_bhc,
                       max_iter_em = 5, incremental = FALSE,
-                      chain = FALSE, ...){
+                      chain_impute = FALSE, ...){
   check_sevt_fit(object)
   if (is.null(data)){
     if (!is.data.frame(object$data_raw)){
       cli::cli_abort(c(
         "{.arg object} must have a data_raw component of class {.cls data.frame}.",
         "x" = "You've supplied {.arg object} where the data_raw component
-      is fo type {.type {object$data_raw}}."
+      is of type {.type {object$data_raw}}."
       ))
     }
     data <- object$data_raw
@@ -44,13 +46,13 @@ stages_em <- function(object, data = object$data_raw, alg_em = stages_bhc,
   object$em <- list()
   start <- copy_sevt(object, c("lambda", "name_unobserved", "em"))
   for(i in 1:max_iter_em){
-    data_c <- impute(data = data, object = object)
+    data_c <- impute(data = data, object = object, chain_impute = chain_impute)
     if (incremental){
       start <- sevt_fit(object, data = data_c)
     } else {
       start <- sevt_fit(start, data = data_c)
     }
-    new <- alg_em(start, ...)
+    new <- stages_alg(start, ...)
     new$em$iter <- i
     if(compare_stages(object, new)){
       object$em$iter <- i
@@ -69,16 +71,16 @@ stages_em <- function(object, data = object$data_raw, alg_em = stages_bhc,
 #' @param data a data.frame
 #' @param object a sevt object
 #' @keywords internal
-impute <- function(data, object, chain = FALSE){
+impute <- function(data, object, chain_impute = FALSE){
   object$skip_checks <- TRUE
   new_data <- data
   for (vv in sevt_varnames(object)){
     wh <- is.na(new_data[[vv]])
     if (sum(wh) > 0){
-      if (isFALSE(chain)){
-        new_data[wh , vv] <- predict(object, class = vv, data[wh,])
+      if (isFALSE(chain_impute)){
+        new_data[wh , vv] <- as.character(predict(object, class = vv, newdata =  data[wh,]))
       } else {
-        new_data[wh , vv] <- predict(object, class = vv, new_data[wh,])
+        new_data[wh , vv] <- as.character(predict(object, class = vv, newdata =  new_data[wh,]))
       }
     }
   }
