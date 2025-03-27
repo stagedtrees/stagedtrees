8e7e274ff2e1a38c6179588760d46a09366082af
gherardo varando <gherardo.varando@gmail.com>
2025-03-27 14:14:17 +0100

    only randomization

--- R/potential_outcomes.R
+++ R/potential_outcomes.R
@@ -1,32 +1,25 @@
 #' Potential Outcomes
 #'
-#' Compute potential outcomes by either randomizing the treatment variable
-#' or via propensity score stratification.
+#' Compute potential outcomes by randomizing
+#' the treatment variable.
 #' @param object a fitted object of class \code{sevt}.
 #' @param outcome the outcome variable.
 #' @param treatment the treatment variable.
-#' @param method string, either \code{"randomization"} or \code{"pstree"}.
 #' @return a matrix with potential outcomes.
 #' @examples
 #' model <- full(Titanic) |> stages_bhc()
 #' potential_outcomes(model, "Survived", "Class", "pstree")
 #' @export
-potential_outcomes <- function(object, outcome, treatment,
-                               method = c("randomization", "pstree", "naive")){
+potential_outcomes <- function(object, outcome, treatment){
   check_sevt_prob(object)
   check_scope(c(outcome, treatment), object)
-  method <- match.arg(method)
-  object0 <- switch(method,
-    randomization = randomize(object, treatment),
-    pstree = ps_tree(object, outcome, treatment),
-    naive = object
-  )
+  object0 <- randomize(object, treatment)
   xx <- c(NA)
   names(xx) <- outcome
   res <- sapply(object$tree[[outcome]], function(vo){
     xx[1] <- vo
     prob(object0, xx, conditional_on = as.data.frame(object$tree[treatment]),
-         na0 = TRUE)
+         na0 = FALSE)
   })
   dimnames(res) <- object$tree[c(treatment, outcome)]
   return(res)
